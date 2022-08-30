f530a68cb7d90eecff8f8a8f863f09a1036dbf14
JDR89 <joaquin.romero89@gmail.com>
2022-08-30 15:53:21 -0300

    correcciones

---
+++ src/data/ProductsMock.js
@@ -0,0 +1,68 @@
+
+    const productos = [
+    {
+      id:1,
+      title: "Remera seleccion XL",
+      price: "$15.000",
+      image: "camiseta.webp",
+      stock: 9,
+    },
+
+    {
+      id: 2,
+      title: "Remera seleccion L",
+      price: "$13.000",
+      image: "camiseta.webp",
+      stock: 6,
+    },
+
+    {
+      id: 3,
+      title: "Campera M",
+      price: "$12.000",
+      image: "campera.webp",
+      stock: 7,
+    },
+
+    {
+      id: 4,
+      title: "Campera S",
+      price: "$10.000",
+      image: "campera.webp",
+      stock: 5,
+    },
+
+    {
+      id: 5,
+      title: "Zapatillas mujer",
+      price: "$15.000",
+      image: "zapas.webp",
+      stock: 3,
+    },
+
+    {
+      id: 6,
+      title: "Zapatillas hombre",
+      price: "$14.000",
+      image: "zapas2.webp",
+      stock: 10,
+    },
+
+    {
+      id: 7,
+      title: "Gorra",
+      price: "$5.000",
+      image: "gorra.webp",
+      stock: 5,
+    },
+
+    {
+      id: 8,
+      title: "Camiseta hockey",
+      price: "$10.000",
+      image: "hockey.webp",
+      stock: 4,
+    },
+  ];
+
+  export default productos
