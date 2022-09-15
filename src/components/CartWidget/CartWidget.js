6c7fce9bf87058fe48c89f4b87ce022d41414259
JDR89 <joaquin.romero89@gmail.com>
2022-09-15 16:00:04 -0300

    desafio cartview

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -3,20 +3,28 @@ import Button from "@mui/material/Button";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../Context/CartContext";
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
 
 const CartWidget = () => {
-   
-  const{getTotalSeleccionados}=useContext(CartContext)
+  const { getTotalSeleccionados, listaProductosCarrito } = useContext(CartContext);
 
   return (
-    <Link to={"/cart"}>
-      <Button className="cart-button" variant="contained">
-        <Badge badgeContent={getTotalSeleccionados()} color="success">
-          <ShoppingCartIcon />
-        </Badge>
-      </Button>
-    </Link>
+    <div>
+        
+        {
+
+        listaProductosCarrito.length>0 && 
+        <>
+          <Link to={"/cart"}>
+            <Button className="cart-button" variant="contained">
+              <Badge badgeContent={getTotalSeleccionados()} color="success">
+                <ShoppingCartIcon />
+              </Badge>
+            </Button>
+          </Link>
+        </>
+      }
+    </div>
   );
 };
 
